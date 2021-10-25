5a4f21d9c556cb8181e87c9ea6abf9ff68b5213b
Sara Ghaedi <saraghaedi75@gmail.com>
2021-10-25 15:52:59 +0200

    fix: change http to https in apiUrl

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,2 +1,2 @@
-export const apiUrl = "http://event-management-project.herokuapp.com";
+export const apiUrl = "https://event-management-project.herokuapp.com";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
